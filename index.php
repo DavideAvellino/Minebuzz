36399e9e78320a8e15e24a3d67f0946338c564d0
DavideAvellino <d06.avellino@gmail.com>
2025-04-30 18:01:17 +0200

    debugging

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="homepage-style.css">
+  <title>Minebuzz</title>
+</head>
+<body>
+
+    {% include "header.php" %}
+
+  <main>
+    <!-- Corpo della pagina (vuoto per ora) -->
+  </main>
+
+    {% include "footer.php" %}
+
+</body>
+</html>
